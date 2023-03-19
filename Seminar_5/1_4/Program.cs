4cbaf71acb41f5a2849f7f7c2bef90b2ce0e1b3d
Kalinin <9776020@mail.ru>
2023-03-19 23:41:44 +0300

    Seminar 5 1_4

---
+++ Seminar_5/1_4/Program.cs
@@ -0,0 +1,42 @@
+﻿int[,] NewMass(int row, int column, int from, int to)
+{
+  int[,] arr = new int[row, column];
+  for (int i = 0; i < row; i++)
+  {
+    for (int j = 0; j < column; j++)
+    {
+      arr[i,j] = new Random().Next(from, to);
+    }
+  }
+  return arr;
+}
+
+void Print(int[,] array)
+{
+  int row = array.GetLength(0);
+  int column = array.GetLength(1);
+  for (int i = 0; i < row; i++)
+  {
+    for (int j = 0; j < column; j++)
+    {
+      Console.Write($" {array[i, j]}");
+    }
+    Console.WriteLine();
+  }
+}
+
+string Find(int[,] array, int find)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      if (array[i,j] == find) return $"{i+1} {j + 1}";
+    }
+  }
+  return "there is no such number";
+}
+
+int[,] new_mass = NewMass(10, 10, 0, 10);
+Print (new_mass);
+Console.WriteLine(Find(new_mass, 11));
