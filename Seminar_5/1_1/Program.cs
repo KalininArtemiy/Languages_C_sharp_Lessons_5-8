986ec024a4d74769d5e46ce054a94e18953480ec
Kalinin <9776020@mail.ru>
2023-03-19 21:45:15 +0300

    Seminar 5 1_1

---
+++ Seminar_5/1_1/Program.cs
@@ -0,0 +1,30 @@
+﻿int[,] NewMass(int row, int column, int from, int to)
+{
+  int[,] arr = new int[row, column];
+  for (int i = 0; i < row; i++)
+  {
+    for (int j = 0; j < column; j++)
+    {
+      arr[i,j] = i+j;
+    }
+  }
+  return arr;
+}
+
+void Print(int[,] array)
+{
+  int row = array.GetLength(0);
+  int column = array.GetLength(1);
+  for (int i = 0; i < row; i++)
+  {
+    for (int j = 0; j < column; j++)
+    {
+      Console.Write($" {array[i, j]}");
+    }
+    Console.WriteLine();
+  }
+}
+
+int[,] new_mass = NewMass(10, 10, 0, 10);
+Print (new_mass);
+
