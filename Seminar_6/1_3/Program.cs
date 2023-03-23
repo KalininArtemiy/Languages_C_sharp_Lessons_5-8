72961766d94674264b8093a9f3a171fe10314fc6
Kalinin <9776020@mail.ru>
2023-03-23 23:02:21 +0300

    Seminar 6 1_2 and 6

---
+++ Seminar_6/1_3/Program.cs
@@ -0,0 +1,27 @@
+﻿int[,] NewMass(int row, int column, int from, int to)
+{
+  int[,] arr = new int[row, column];
+  for (int i = 0; i < row; i++)
+  {
+    for (int j = 0; j < column; j++)
+    {
+      arr[i,j] = new Random().Next(from, to);
+    }
+  }
+  return arr;
+}
+
+void Print(int[,] array)
+{
+  int row = array.GetLength(0);
+  int column = array.GetLength(1);
+  for (int i = 0; i < row; i++)
+  {
+    for (int j = 0; j < column; j++)
+    {
+      Console.Write($" {array[i, j]}");
+    }
+    Console.WriteLine();
+  }
+  Console.WriteLine();
+}
