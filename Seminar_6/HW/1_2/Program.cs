ac74ea7be93f492e889befed930bfffb6ea5ccab
Kalinin <9776020@mail.ru>
2023-03-25 23:04:01 +0300

    Seminar 6 HW 2

---
+++ Seminar_6/HW/1_2/Program.cs
@@ -0,0 +1,52 @@
+﻿int[,] NewMass(int row, int column, int from, int to)
+{
+  int[,] arr = new int[row, column];
+  for (int i = 0; i < row; i++)
+  {
+    for (int j = 0; j < column; j++)
+    {
+      arr[i,j] = new Random().Next(from, to);
+    }
+  }
+  return arr;
+}
+
+void Print(int[,] array)
+{
+  int row = array.GetLength(0);
+  int column = array.GetLength(1);
+  for (int i = 0; i < row; i++)
+  {
+    for (int j = 0; j < column; j++)
+    {
+      Console.Write($" {array[i, j]}");
+    }
+    Console.WriteLine();
+  }
+}
+void SortToLower(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      for (int l = 0; l < array.GetLength(1) - 1; l++)
+      {
+        if (array[i, l] < array[i, l + 1])
+        {
+          (array[i,l],array[i, l + 1]) = (array[i, l + 1],array[i,l]);
+        }
+      }
+    }
+  }
+}
+
+int num_row = int.Parse(Console.ReadLine()!);
+int num_column = int.Parse(Console.ReadLine()!);
+int from = int.Parse(Console.ReadLine()!);
+int to = int.Parse(Console.ReadLine()!);
+int[,] mass = NewMass(num_row, num_column, from, to);
+Print(mass);
+Console.WriteLine();
+SortToLower(mass);
+Print(mass);
