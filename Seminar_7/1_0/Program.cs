37c9bab95a77cf007ee275c2c6fa76b5ed866051
Kalinin <9776020@mail.ru>
2023-03-31 01:33:32 +0300

    Seminar_7_0_1

--- Seminar_7/1_0/Program.cs
+++ Seminar_7/1_0/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void ValuesSet(int N)
+{
+  if (N == 0) return;
+  ValuesSet (N - 1);
+  Console.Write($"{N} ");
+}
+
+int num = int.Parse(Console.ReadLine());
+ValuesSet(num);
