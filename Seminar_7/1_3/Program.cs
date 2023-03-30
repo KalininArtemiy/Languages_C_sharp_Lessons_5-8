0411353e00f01427a5c453774c8419a788979fc2
Kalinin <9776020@mail.ru>
2023-03-31 02:20:36 +0300

    Seminar_7_3

---
+++ Seminar_7/1_3/Program.cs
@@ -0,0 +1,8 @@
+﻿int ValuesSet(int A, int B)
+{
+  if (B < 1) return 1;
+  return ValuesSet (A, B - 1) * A ; 
+}
+int num1 = int.Parse(Console.ReadLine()!);
+int num2 = int.Parse(Console.ReadLine()!);
+Console.WriteLine(ValuesSet(num1, num2));
