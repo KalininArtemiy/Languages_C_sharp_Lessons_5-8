6ba4c13ee9dae00974f24a3e27744ebdab33cc07
Kalinin <9776020@mail.ru>
2023-03-31 01:38:18 +0300

    Seminar_7_1

---
+++ Seminar_7/1_1/Program.cs
@@ -0,0 +1,9 @@
+﻿void ValuesSet(int N, int M)
+{
+  if (N < M) return;
+  ValuesSet(N - 1, M);
+  Console.Write ($"{N} ");
+}
+int num1 = int.Parse(Console.ReadLine()!);
+int num2 = int.Parse(Console.ReadLine()!);
+ValuesSet(num1, num2);
